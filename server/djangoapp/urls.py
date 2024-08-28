649acf17689187893a7910f00aab22a5042f1d71
HisanaFaheem <hisanaabbas2031@gmail.com>
2024-08-28 14:43:18 -0400

    Added user management and static pages

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,10 +11,14 @@ urlpatterns = [
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
- path('login/', TemplateView.as_view(template_name="index.html")),
-  path('logout/', views.logout_view, name='logout'),
-     path('register/', TemplateView.as_view(template_name="index.html")),
-    # path for dealer reviews view
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', TemplateView.as_view(template_name="index.html")),
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+      path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+      path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+      # path for dealer reviews view
 
     # path for add a review view
 
